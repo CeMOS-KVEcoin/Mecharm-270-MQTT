d01f97f8fd24941d7dc061276f6c29c3a046d051
Rahel <rahel.grandi@web.de>
2025-12-01 15:08:17 +0100

    a

--- skills.py
+++ skills.py
@@ -26,10 +26,10 @@ def release_conveyor2(robot, vacuum, speed=40):
     robot.home(speed)
 
 def put_pedastel(robot, vacuum, speed=40):
-    robot.move_angles([-10, 0, 0, 0, 0, -150], speed)
+    robot.move_angles([-10, 0, 0, 0, 0, -179], speed)
     time.sleep(2)
 
-    robot.move_angles([-10, 0, 10, 0, -10, -150], speed)
+    robot.move_angles([-10, 0, 10, 0, -10, -179], speed)
     time.sleep(2)
 
     vacuum.off()
