667f4cff2e50a295cb2e7c38b271de6ae4377ebc
Rahel <rahel.grandi@web.de>
2026-08-07 11:56:56 +0200

    laser coords

--- skills.py
+++ skills.py
@@ -132,25 +132,32 @@ def pickupFromConveyor2(speed=40):
 # [161.45,34.1,26.27,-8.87,-68.55,-87.97]
 # [161.36,92.02,-80.85,-1.14,-13.53,-85.78] -> J2 hoch
 def placeToLaser(speed=40):
-    robot.send_angle(Angle.J1.value, 159.9, speed)
+    moveTo([0, 0, -30, 0, 0, 159.9], speed)
     time.sleep(1)
     moveTo([159.9, 30, 20, 0, -60, -90], speed)
     time.sleep(1)
-    moveTo([159.9, 90, -80, 0, -60, -90], speed)
+    moveTo([159.9, 90, -80, 0, -10, -90], speed)
     time.sleep(1)
     release()
     time.sleep(2)
+    moveTo([159.9, 30, 20, 0, -60, -90], speed)
+    time.sleep(1)
+    moveTo([0, 0, -30, 0, 0, 159.9], speed)
+    time.sleep(1)
     home(speed)
 
 def pickupFromLaser(speed=40):
-    home(speed)
-    robot.send_angle(Angle.J1.value, 159.9, speed)
+    moveTo([0, 0, -30, 0, 0, 159.9], speed)
     time.sleep(1)
     moveTo([159.9, 30, 20, 0, -60, -90], speed)
     time.sleep(1)
-    moveTo([159.9, 90, -80, 0, -60, -90], speed)
+    moveTo([159.9, 90, -80, 0, -10, -90], speed)
     time.sleep(1)
     grip()
+    time.sleep(1)
+    moveTo([159.9, 30, 20, 0, -60, -90], speed)
+    time.sleep(1)
+    moveTo([0, 0, -30, 0, 0, 159.9], speed)
     time.sleep(1)
     home(speed)
 
